0e7639b838b3dc09bc1b8680684ad73daf7eb6b5
Beddaniel <dabednarz243@gmail.com>
2023-12-18 09:33:23 +0100

    codewars-Rot13_5kyu

--- codewars-Rot13_5kyu.cpp
+++ codewars-Rot13_5kyu.cpp
@@ -1,3 +1,11 @@
+/*
+ROT13 is a simple letter substitution cipher that replaces a letter with the letter 13 letters after it in the alphabet. 
+ROT13 is an example of the Caesar cipher.
+Create a function that takes a string and returns the string ciphered with Rot13. 
+If there are numbers or special characters included in the string, they should be returned as they are. 
+Only letters from the latin/english alphabet should be shifted, like in the original Rot13 "implementation".
+*/
+
 #include <string>
 #include <iostream>
 
@@ -46,3 +54,34 @@ int main(){
 
     return 0;
 }
+
+/* The most clever solutions:
+
+std::string rot13(std::string msg)
+{
+  for(auto& x : msg) 
+    if (islower(x)) x = 'a'+(x-'a'+13)%26; 
+    else if(isupper(x)) x = 'A'+(x-'A'+13)%26;
+  return msg;
+}
+
+
+std::string rot13(std::string msg) {
+  for (char& c : msg) {
+    switch (c) {
+      case 'A'...'M': case 'a'...'m': c += 13; break;
+      case 'N'...'Z': case 'n'...'z': c -= 13; break;
+    }
+  }
+  return msg;
+}
+
+std::string rot13(std::string msg)
+{
+    for (char &c : msg)
+        if (::isalpha(c))
+            c += 13 * ((::tolower(c) < 'n') * 2 - 1);
+    return msg;
+}
+
+*/
