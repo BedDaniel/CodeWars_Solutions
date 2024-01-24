6989b0d4ecb12ee292e6ae08d44dfdc82d80aa5f
BedDaniel <dabednarz243@gmail.com>
2024-01-24 13:20:24 +0100

    codewars-Pick_peaks_5kyu

---
+++ codewars-Pick_peaks_5kyu.cpp
@@ -0,0 +1,92 @@
+/*
+In this kata, you will write a function that returns the positions and the values of the "peaks" (or local maxima) of a numeric array.
+For example, the array arr = [0, 1, 2, 5, 1, 0] has a peak at position 3 with a value of 5 (since arr[3] equals 5).
+
+The output will be returned as an object of type PeakData which has two members: pos and peaks. Both of these members should be vector<int>s. If there is no peak in the given array then the output should be a PeakData with an empty vector for both the pos and peaks members.
+
+PeakData is defined in Preloaded as follows:
+
+struct PeakData {
+  vector<int> pos, peaks;
+};
+
+Example: pickPeaks([3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3]) should return {pos: [3, 7], peaks: [6, 3]} (or equivalent in other languages)
+All input arrays will be valid integer arrays (although it could still be empty), so you won't need to validate the input.
+
+The first and last elements of the array will not be considered as peaks (in the context of a mathematical function, we don't know what is after and before and therefore, we don't know if it is a peak or not).
+
+Also, beware of plateaus !!! [1, 2, 2, 2, 1] has a peak while [1, 2, 2, 2, 3] and [1, 2, 2, 2, 2] do not. In case of a plateau-peak, please only return the position and value of the beginning of the plateau. For example: pickPeaks([1, 2, 2, 2, 1]) returns {pos: [1], peaks: [2]} (or equivalent in other languages)
+
+Have fun!
+
+*/
+
+
+
+#include <iostream>
+#include <vector>
+struct PeakData {
+	std::vector<int> pos, peaks;
+};
+
+PeakData pick_peaks(const std::vector<int>& v) {
+	PeakData result;
+	int possiblePos = 0;
+	bool possiblePlateu = false;
+	int size = v.size();
+
+	if (v.empty()) {
+		return result;
+	}
+
+	for (int i = 1; i < size - 1; ++i)
+	{
+		if (v[i - 1] < v[i] and v[i] > v[i + 1])
+		{
+			result.pos.push_back(i);
+			result.peaks.push_back(v[i]);
+		}
+		else if (v[i - 1] < v[i] and v[i] == v[i + 1])
+		{
+			possiblePlateu = true;
+			possiblePos = i;
+		}
+		else if (v[i - 1] == v[i] and v[i] > v[i + 1] and possiblePlateu == true)
+		{
+			result.pos.push_back(possiblePos);
+			result.peaks.push_back(v[possiblePos]);
+			possiblePlateu = false;
+		}
+
+		else if (v[i - 1] == v[i] and v[i] < v[i + 1])
+		{
+			possiblePlateu = false;
+		}
+	}
+	return result;
+}
+
+int main() {
+	std::vector<int> vec = { 3, 2, 3, 6, 4, 1, 2, 3, 2, 1, 2, 3 };
+	pick_peaks(vec);
+
+}
+
+/*
+The most clever solution:
+
+PeakData pick_peaks(vector<int> v) {
+	PeakData result;
+	int i, t;
+	for (t=0, i=1; i<v.size(); i++)
+	{
+		if (v[i]>v[i-1]) { t=i; }
+
+		else if (t && v[i]<v[i-1])
+		{
+			result.pos.push_back(t), result.peaks.push_back(v[t]), t=0;
+		}
+	}
+	return result;
+}
+*/
